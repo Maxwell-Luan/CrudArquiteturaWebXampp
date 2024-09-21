28429fa0d9208b45576e3a8732b5542cbff5d6ed
Maxwell L. <85193416+Maxwell-Luan@users.noreply.github.com>
2024-09-20 22:06:59 -0300

    Adicionando arquivo para salvar para a aula

---
+++ CrudArquiteturaWebXampp/CrudArquiteturaWeb-main/crud/src/main/java/com/api/crud/models/Employee.java
@@ -0,0 +1,30 @@
+package com.api.crud.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    private Date birthdate;
+    private String cv;
+}
